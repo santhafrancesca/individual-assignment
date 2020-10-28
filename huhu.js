60ee53d26fda2da5b6d58a6a291aeaa1b4b21d52
santhafrancesca <69284718+santhafrancesca@users.noreply.github.com>
2020-10-28 10:54:37 +0800

    Add files via upload

---
+++ huhu.js
@@ -0,0 +1,47 @@
+function StartOrStop(audioFile) {
+    var audio = document.getElementById("myAudio");
+    if (!audio.src || audio.src !== audioFile) audio.src = audioFile;
+    if (audio.paused == false)
+         audio.pause();
+    else
+        audio.play();
+}
+    
+function StartOrStop(audioFile) {
+    var audio = document.getElementById("myAudio2");
+    if (!audio.src || audio.src !== audioFile) audio.src = audioFile;
+    if (audio.paused == false)
+    audio.pause();
+    else
+        audio.play();
+        
+}
+function StartOrStop(audioFile) {
+    var audio = document.getElementById("myAudio3");
+    if (!audio.src || audio.src !== audioFile) audio.src = audioFile;
+    if (audio.paused == false)
+    audio.pause();
+    else
+        audio.play();
+       
+}
+function StartOrStop(audioFile) {
+    var audio = document.getElementById("myAudio4");
+    if (!audio.src || audio.src !== audioFile) audio.src = audioFile;
+    if (audio.paused == false)
+    audio.pause();
+    else
+        audio.play();
+}
+function newDoc() {
+window.location.assign("assig2.html")
+}
+function newDoc2() {
+window.location.assign("assig1.html")
+}
+function newDoc3() {
+window.location.assign("assig3.html")
+}
+function newDoc4() {
+window.location.assign("assig4.html")
+}
